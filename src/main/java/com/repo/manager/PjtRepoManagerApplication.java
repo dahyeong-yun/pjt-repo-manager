65ff0f8637df5296209566fac7f58e0bbbf1e968
danny <gryb809@gmail.com>
2022-07-20 14:27:54 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/repo/manager/PjtRepoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.repo.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjtRepoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjtRepoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/repo/manager/PjtRepoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjtRepoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
