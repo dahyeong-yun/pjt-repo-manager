682bf7c6350acfb7cf3a1230f4434881c0da2ef7
danny <gryb809@gmail.com>
2022-07-26 15:48:15 +0900

    :sparkles: oauth2login successHandler 적용

---
+++ src/main/java/com/repo/manager/config/OAuth2SuccessHandler.java
@@ -0,0 +1,25 @@
+package com.repo.manager.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println(objectMapper.writeValueAsString(authentication));
+    }
+}
--- src/main/java/com/repo/manager/config/SecurityConfig.java
+++ src/main/java/com/repo/manager/config/SecurityConfig.java
@@ -1,21 +1,25 @@
 package com.repo.manager.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
             .anyRequest().authenticated()
         .and()
-            .oauth2Login();
+            .oauth2Login()
+                .successHandler(oAuth2SuccessHandler); // 인증 서버에서 인증이 성공하는 경우
         return http.build();
     }
 }
